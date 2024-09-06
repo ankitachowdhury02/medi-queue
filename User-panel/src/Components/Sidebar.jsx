e24b729ab7aee857998063bb08ef19fcbdd51913
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2024-09-06 08:15:08 -0700

    hello

--- User-panel/src/Components/Sidebar.jsx
+++ User-panel/src/Components/Sidebar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
     <div className='bg-gradient-to-r from-[#9083D5] to-[#807593] sticky top-0 h-[100vh] hidden sm:block w-[25%]'>
-      <div className='w-full h-20 flex items-center justify-center'>
+      <div className='w-full h-16 flex items-center justify-center'>
         <h1 className='bold text-white'>Medi-Queue</h1>
       </div>
-      <div>
-        
+      <div className='h-full flex flex-col '>
+        <div className=' h-3 w-full mt-4 p-5  flex items-center justify-center '><Link className='text-xl text-white' to="/">Home</Link></div>
+        <div className=' h-3 w-full mt-4 p-5  flex items-center justify-center '><Link className='text-xl text-white' to="/appointment">Appointment</Link></div>
+        <div className=' h-3 w-full mt-4 p-5  flex items-center justify-center '><Link className='text-xl text-white' to="/update-profile">Profile Update</Link></div>
+        <div className=' h-3 w-full mt-4 p-5  flex items-center justify-center '><Link className='text-xl text-white' to="/help-support">Help And Support</Link></div>
       </div>
     </div>
   )
--- User-panel/src/Routes/Routes.jsx
+++ User-panel/src/Routes/Routes.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import Signup from "../Components/Signup";
+import Appointment from "../Pages/Appointment";
+import ProfileUpdate from "../Pages/ProfileUpdate";
+import HelpAndSupport from "../Pages/HelpAndSupport";
 
 
 const router = createBrowserRouter([
@@ -13,6 +16,18 @@ const router = createBrowserRouter([
                 path:"/",
                 element:<Home/>
             },
+            {
+                path: "/appointment",
+                element: <Appointment/>
+            },
+            {
+                path: "/update-profile",
+                element: <ProfileUpdate/>
+            },
+            {
+                path: "/help-support",
+                element: <HelpAndSupport/>
+            }
         ]
     },
     {
