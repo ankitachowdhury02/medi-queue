b98060371ede54b94c75771a3b7c9a293d80b5ad
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2024-09-11 23:47:41 -0700

    new

---
+++ frontend/src/Components/User-Components/UserLogIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserLogIn = () => {
+  return (
+    <div>UserLogIn</div>
+  )
+}
+
+export default UserLogIn
--- frontend/src/Routes/Router.jsx
+++ frontend/src/Routes/Router.jsx
@@ -4,31 +4,67 @@ import Home from "../Pages/User-Pages/Home";
 import Appointment from "../Pages/User-Pages/Appointment";
 import ProfileUpdate from "../Pages/User-Pages/ProfileUpdate";
 import HelpAndSupport from "../Pages/User-Pages/HelpAndSupport";
+import Hospital from "../Layout/Hospital";
+import HospitalHome from "../Pages/Hospital-pages/HospitalHome";
+import AppointmentDetails from "../Pages/Hospital-pages/AppointmentDetails";
+import Portfolio from "../Pages/Portfolio/Portfolio";
+import UserLogIn from "../Components/User-Components/UserLogIn";
+import HospitalLogin from "../Components/Hospital-Components/HospitalLogin";
 
 
 const router = createBrowserRouter([
+    
     {
-        path:"/",
+        path: "/",
+        element: <Portfolio/>,
+    },
+    {
+        path: "/user-login",
+        element: <UserLogIn/>
+    },
+    {
+        path:"/user",
         element:<Main/>,
         children: [
             {
-                path:"/",
+                path:"/user",
                 element:<Home/>
             },
             {
-                path: "/appointment",
+                path: "/user/appointment",
                 element: <Appointment/>
             },
             {
-                path: "/update-profile",
+                path: "/user/update-profile",
                 element: <ProfileUpdate/>
             },
             {
-                path: "/help-support",
+                path: "/user/help-support",
                 element: <HelpAndSupport/>
             }
         ]
     },
+    {
+        path: "/hospital-login",
+        element: <HospitalLogin/>
+    },
+    {
+        path: "/hospital",
+        element:<Hospital/>,
+        children: [
+            {
+                path: "/hospital",
+                element: <HospitalHome/>
+            },
+            {
+                path: "/hospital/appointments",
+                element: <AppointmentDetails/>
+            },
+            {
+                path: "/hospital/bedbooking"
+            }
+        ]
+    }
    
 ])
 
